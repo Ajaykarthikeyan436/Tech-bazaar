4e664e03954707e6cc705c2b50268d67da2b3591
unknown <ajaykarthikeyan436@gmail.com>
2025-06-24 16:44:42 +0530

    URl Added

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ const connectDB = require("./config/mongodb.js");
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174','https://tech-bazaar-frontend-weld.vercel.app'],
+  origin: ['http://localhost:5173', 'http://localhost:5174','https://tech-bazaar-frontend-e9yp3w3nu-ajays-projects-8ce49744.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'], // keep 'Authorization'
   credentials: true,
